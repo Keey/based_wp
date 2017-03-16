415de4cc1d19f76ceceddd30e60e997091141a88
yamaksum <yamaksum@gmail.com>
2017-03-16 10:52:20 +0200

    --

--- js/init.js
+++ js/init.js
@@ -1,10 +1,2 @@
-$(document).ready(function () {
-    $(this).on('click', '.wpcf7-not-valid-tip', function(){
-        $(this).prev().trigger('focus');
-        $(this).fadeOut(500,function(){
-            $(this).remove();
-        });
-    });
-    FastClick.attach(document.body);
-});
+$(document).ready(function () {});
 
