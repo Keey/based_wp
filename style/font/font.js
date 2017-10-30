74dae5b65603df2c564cfb874d8e81dc95220d0c
Yamaksum <yamaksum@gmail.com>
2017-10-30 10:21:19 +0200

    Update font.js

--- style/font/font.js
+++ style/font/font.js
@@ -1,10 +1,5 @@
 !function () {
     var nua = navigator.userAgent;
-    var noSupport = !window.addEventListener // IE8 и ниже
-        || (nua.match(/(Android (2|3|4.0|4.1|4.2|4.3))|(Opera (Mini|Mobi))/) && !nua.match(/Chrome/)) // Android Stock Browser до 4.4 и Opera Mini
-    if (noSupport) {
-        return;
-    }
     function e(e, t, n) {
         e.addEventListener ? e.addEventListener(t, n, !1) : e.attachEvent && e.attachEvent("on" + t, n)
     }
@@ -21,10 +16,6 @@
             var a = document.createElement("link");
             a.href = o, a.rel = "stylesheet", a.type = "text/css", document.getElementsByTagName("head")[0].appendChild(a), document.cookie = "font_css_cache"
         }
-    }
-    function c(e) {
-        var t = document.createElement("style");
-        t.innerHTML = e, document.getElementsByTagName("head")[0].appendChild(t)
     }
     var o = "font.css"; //url file font convert base64
     window.localStorage && localStorage.font_css_cache || document.cookie.indexOf("font_css_cache") > -1 ? n() : e(window, "load", n)
