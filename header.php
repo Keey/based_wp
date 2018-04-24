3d8301dee687b73d51a7d234f4e64c5cc957fdf3
Yamaksum <yamaksum@gmail.com>
2018-04-24 09:53:53 +0300

    Update header.php

--- header.php
+++ header.php
@@ -14,8 +14,6 @@ header('X-UA-Compatible: IE=Edge,chrome=1');
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
     <meta name="format-detection" content="telephone=no">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-          crossorigin="anonymous">
     <?php wp_head(); ?>
     <?php if (is_singular()) wp_enqueue_script('comment-reply'); ?>
 </head>
