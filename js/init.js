b4d622f9187903cda16a7e146d86b4273b733505
Yamaksum <yamaksum@gmail.com>
2018-08-24 14:48:19 +0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,2 +1,22 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+    //swiper_example
+    //     new Swiper('.slider_name', {
+    //     loop: true,
+    //     speed: 1000,
+    //     parallax:true,
+    //     autoplay: {
+    //         delay: 5000
+    //     },
+    //     pagination: {
+    //         el: '.swiper-pagination',
+    //         dynamicBullets: true,
+    //         clickable: true
+    //     },
+    //     navigation: {
+    //         prevEl: '.swiper-prev',
+    //         nextEl: '.swiper-next'
+    //     }
+    // });
+
+});
 
