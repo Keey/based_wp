c276b88f807d9b039ae907dcbc9246f0184c0167
Yamaksum <yamaksum@gmail.com>
2018-09-10 15:01:37 +0300

    Update lib.js

--- js/lib.js
+++ js/lib.js
@@ -4,30 +4,7 @@ var onReCaptchaSuccess = function() {
 		$('html, body').animate({ scrollTop: destElementOffset }, 0);
 	}
 }
-//selbel
-(function ($) {
-    'use strict';
-    $.fn.selbel = function(settings) {
-        var defaults = {
-            onChange: function () {}
-        },
-        options = $.extend(defaults, settings);
 
-        return this.each(function () {
-            var el = $(this),
-                sb_label = el.attr("data-label") !== undefined ? '<label>' + el.attr("data-label") + '</label>' : '';
-            if (!el.hasClass('selbel')) { el.addClass('selbel'); }
-            if(el.parent().is('.selbel_w')) { return false; }
-            el.each(function() {
-                $(this).wrap("<span class='selbel_w' />").before(sb_label).after('<span>' + $('*:selected', this).text() + '</span>');
-            });
-            el.change(function() {
-                $(this).next().text($('*:selected', this).text());
-                if(options.onChange) options.onChange.call(this);
-            });
-        });
-    };
-})(jQuery);
 //find empty paragraphs
 $('p').each(function() {
     var t = $(this);
