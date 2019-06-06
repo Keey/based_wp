0d4e332e4bbd1766a883436ca80da15b88185761
Yamaksum <yamaksum@gmail.com>
2019-06-06 09:34:37 +0300

    Update header.php

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + 3600));
 header('Content-Type: text/html; charset=utf-8');
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-header('X-UA-Compatible: IE=Edge,chrome=1');
 ?>
 <!doctype html>
 <html lang="en">
@@ -23,7 +22,7 @@ header('X-UA-Compatible: IE=Edge,chrome=1');
     <div class="row">
         <a href="<?php echo site_url(); ?>/" class="logo"></a>
         <nav>
-            <?php wp_nav_menu(array('container' => false, 'items_wrap' => '<ul id="%1$s" class="box">%3$s</ul>', 'theme_location' => 'head_menu')); ?>
+            <?php wp_nav_menu(array('container' => false, 'items_wrap' => '<ul class="box">%3$s</ul>', 'theme_location' => 'head_menu')); ?>
         </nav>
     </div>
 </header>
